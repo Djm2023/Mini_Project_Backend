d17af990aa921ee1803b0aadb97907868df08ab2
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-10-06 20:38:40 +0530

    Make UI and hard coded the data

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const port = 8000;
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('First step towards learning Express framework of Node.js');
-})
 
-app.listen(port , (err) => {
-    if(err){
-        console.log("Error in running the server");
-    }
-    console.log(`Server is up and running on port ::::${port}`);
-})
+
+app.get("/", (req, res) => {
+  res.send("First step towards learning Express framework of Node.js");
+});
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Error in running the server");
+  }
+  console.log(`Server is up and running on port ::::${port}`);
+});
