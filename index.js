14d50204cc9cb995bd0a74fa26bb612d69b3034c
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-10-08 21:21:36 +0530

    Make a Add product page

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import express from 'express';
+import express from "express";
 import ProductController from "./src/controllers/product.controller.js";
-import path from 'path';
-import expressEjsLayouts from 'express-ejs-layouts';
+import path from "path";
+import expressEjsLayouts from "express-ejs-layouts";
 const app = express();
 const port = 8000;
 
-app.set("view engine" , "ejs");
-app.set("views" , path.join(path.resolve() , 'src', 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(path.resolve(), "src", "views"));
 
 app.use(expressEjsLayouts);
 
 const productController = new ProductController();
 
+app.use(express.static("src/views"));
 
 app.get("/", productController.getProducts);
-app.use(express.static("src/views"));
+
+app.get('/new' , productController.getAddForms);
 
 app.listen(port, (err) => {
   if (err) {
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,13 @@
-import path from "path";
 import ProductModel from "../models/product.model.js";
 
 export default class ProductController {
   getProducts(req, res) {
     let products = ProductModel.get();
     console.log(products);
-    // 
-    res.render("product" , {product:products})
+    //
+    res.render("product", { product: products });
+  }
+  getAddForms(req, res) {
+    return res.render("new-product-form");
   }
 }
